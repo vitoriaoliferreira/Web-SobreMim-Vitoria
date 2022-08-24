56b2a580259f274fbd0568c7c656dc28d417976d
Vitoria Ferreira <ferreiravitoriamaria22@gmail.com>
2022-08-23 21:18:06 -0300

    Versão Final

---
+++ script.js
@@ -0,0 +1,17 @@
+
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.banner-curiosidades', {duration: 1000});
+
+sr.reveal('.banner-bandas', {duration: 1000});
+
+sr.reveal('.banner-jogos', {duration: 1000});
+
+sr.reveal('.banner-series', {duration: 1000});
+
+sr.reveal('.banner-leituras', {duration: 1000});
+
+sr.reveal('.instagram', {duration: 1000});
+
+sr.reveal('.footer', {duration: 1000});
+
